3757addbe1dfab1d931a61e0af087f31fb62f2a6
Amadougaye Cissé <agcisse@Amadougayes-MacBook-Pro.local>
2022-04-11 17:32:07 +0200

    step-21

--- lib/views/notes/create_update_note_view.dart
+++ lib/views/notes/create_update_note_view.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:share_plus/share_plus.dart';
 import 'package:taskify/services/auth/auth_service.dart';
 import 'package:taskify/utilities/generics/get_arguments.dart';
 import '../../services/cloud/cloud_note.dart';
 import '../../services/cloud/firebase_cloud_storage.dart';
+import '../../utilities/dialogs/cannot_share_empty_note_dialog.dart';
 
 class CreateUpdateNoteView extends StatefulWidget {
   const CreateUpdateNoteView({Key? key}) : super(key: key);
@@ -96,7 +98,22 @@ class _CreateUpdateNoteViewState extends State<CreateUpdateNoteView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('New note')),
+      appBar: AppBar(
+        title: const Text('New note'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              final text = _textController.text;
+              if (_note == null || text.isEmpty) {
+                showCannotShareEmptyNoteDialog(context);
+              } else {
+                Share.share(text);
+              }
+            },
+            icon: const Icon(Icons.share),
+          )
+        ],
+      ),
       body: FutureBuilder(
         future: createOrGetExistingNote(context),
         builder: (context, snapshot) {
